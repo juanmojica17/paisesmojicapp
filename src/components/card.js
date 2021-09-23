ccd3a9274fca6c049e559bc177a1593e41edeff2
juanmojica17 <juanmojicaayure@gmail.com>
2021-09-22 21:46:32 -0400

    fastacces

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Countries from "../views/countries"
 import "./styles/card.css"
-
+import { Link } from "react-router-dom";
 const Card =({name, key, flag})=>{
     return(
         
         <div className="card">
             <h2>{name}</h2>
-            <div >
-            <img className="flag" src={flag}/>
-            </div>
+            
+            <Link to={`/Country/${name}`}><img className="flag" src={flag}/></Link>
+            
         </div>
         
     )
